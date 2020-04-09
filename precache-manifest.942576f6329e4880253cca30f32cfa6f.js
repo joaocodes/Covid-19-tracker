f0d5e847155fb467bf895179570c88e2fe37c7ec
joaocodes <joaojunger1@gmail.com>
2020-04-09 01:20:25 -0400

    Updates

---
+++ precache-manifest.942576f6329e4880253cca30f32cfa6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec816c4115966b9e1440a42145e0f14",
+    "url": "/Covid-19-tracker/index.html"
+  },
+  {
+    "revision": "03438a49a1cfa392035e",
+    "url": "/Covid-19-tracker/static/css/main.4d4b38d8.chunk.css"
+  },
+  {
+    "revision": "33de40b35c4036270404",
+    "url": "/Covid-19-tracker/static/js/2.3dba0d3b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-tracker/static/js/2.3dba0d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03438a49a1cfa392035e",
+    "url": "/Covid-19-tracker/static/js/main.9458d46d.chunk.js"
+  },
+  {
+    "revision": "4bd1dd0dbb15287dc37a",
+    "url": "/Covid-19-tracker/static/js/runtime-main.43c98563.js"
+  },
+  {
+    "revision": "8c4bc078451756628d83d24acb87dff8",
+    "url": "/Covid-19-tracker/static/media/covidBanner.8c4bc078.png"
+  }
+]);
